5941338d7dcea0454de041387930df2fbd7aa94c
Oscar Figueroa <fisocodes@gmail.com>
2021-09-03 10:34:11 -0700

    Token implemented

--- routes/users.js
+++ routes/users.js
@@ -14,27 +14,32 @@ router.post('/authenticate', function(req, res){
 
 router.post('/create', function(req, res){
 
-    bcrypt.hash(req.body.password, 14, (e, hash) => {
-        req.body.password = hash
-        
-        try{
-            mongoose.connect("mongodb+srv://fisocodes:sotooscar1@dandocluster.8qole.mongodb.net/dando-database?retryWrites=true&w=majority", {useNewUrlParser: true}, () => {
-                const user = new User(req.body);
-                user.save(e => {
-                    if(e){
-                        console.log(e);
-                        res.status(500).send(e.message);
-                    }
-                    else
-                        res.status(201).send("User created succesfully");
-                }); 
-            });        
-        }
-        catch(e) {
-            console.log(e.message);
-            res.status(500).send(e.message);
-        }
-    });
+    if(req.body.token !== "0420"){
+        res.status(500).send("Not a valid token");
+    }
+    else{
+        bcrypt.hash(req.body.password, 14, (e, hash) => {
+            req.body.password = hash
+            
+            try{
+                mongoose.connect("mongodb+srv://fisocodes:sotooscar1@dandocluster.8qole.mongodb.net/dando-database?retryWrites=true&w=majority", {useNewUrlParser: true}, () => {
+                    const user = new User(req.body);
+                    user.save(e => {
+                        if(e){
+                            console.log(e);
+                            res.status(500).send(e.message);
+                        }
+                        else
+                            res.status(201).send("User created succesfully");
+                    }); 
+                });        
+            }
+            catch(e) {
+                console.log(e.message);
+                res.status(500).send(e.message);
+            }
+        });
+    }
 });
 
 router.put('/update', function(req, res){
