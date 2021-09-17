031977c2d2bc4bd60da5f11562471c4058383b66
Oscar Figueroa <fisocodes@gmail.com>
2021-09-17 14:43:01 -0700

    Fixes to cors and cookies

--- app.js
+++ app.js
@@ -5,24 +5,7 @@ const users = require('./routes/users');
 
 const app = express();
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors({origin: 'http://locahost:3000', credentials: true}));
 
 app.use('/users', users);
 
