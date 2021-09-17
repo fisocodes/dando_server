3c1dc153c4790c42f8e9b077c23ed9202b700a8a
Oscar Figueroa <fisocodes@gmail.com>
2021-09-17 15:48:06 -0700

    Adding dando to cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const users = require('./routes/users');
 
 const app = express();
 
-app.use(cors({origin: ['http://localhost:3000', 'https://dando.netlify.app/'], credentials: true}));
+app.use(cors({origin: 'https://dando.netlify.app', credentials: true}));
 
 app.use('/users', users);
 
