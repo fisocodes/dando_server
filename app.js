a67c5d2be437bc03902a59dee607452abfd3ccd4
Oscar Figueroa <fisocodes@gmail.com>
2021-09-17 16:39:09 -0700

    changes to cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const users = require('./routes/users');
 
 const app = express();
 
-app.use(cors({origin: ["https://dando.netlify.app", "http://localhost:3000"], credentials: true, methods: ['GET', 'POST', 'UPDATE', 'DELETE']}));
+app.use(cors({origin: true, credentials: true}));
 
 app.use('/users', users);
 
