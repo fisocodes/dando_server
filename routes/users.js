4b40da1650767a3d0854989889d049a4b46e69c6
Oscar Figueroa <fisocodes@gmail.com>
2021-09-17 12:50:38 -0700

    Sessions implemented

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const connect = require('connect');
 const router = express.Router();
 const bcrypt =  require('bcrypt');
 const mongoose = require('mongoose');
 const passport =  require('passport');
 const LocalStrategy =  require('passport-local').Strategy;
+const session =  require('express-session');
+const MongoDBStore =  require('connect-mongodb-session')(session);
 
 const User = require('../database/Models/user');
+
+const store = new MongoDBStore({
+    uri: "mongodb+srv://fisocodes:sotooscar1@dandocluster.8qole.mongodb.net/dando-database?retryWrites=true&w=majority",
+    collection: 'sessions'
+});
+
+router.use(session({
+    secret: 'the secret',
+    cookie: {maxAge: 2000 * 60},
+    store: store,
+    resave: true,
+    saveUninitialized: true
+}));
 
 passport.use(new LocalStrategy(
     function(username, password, done){
@@ -32,29 +48,41 @@ passport.use(new LocalStrategy(
 ));
 
 router.use(passport.initialize());
+router.use(passport.session());
 
 router.use(express.json());
 
 
 router.post('/authenticate', function(req, res, next){
-    passport.authenticate('local', {session: false}, function(e, user, info){
-        if(e){
-            res.send(e.message);
-        }
+    
+    console.log(req.session);
 
-        if(!user){
-            res.status(500).send('User does not exist');
-        }
-        else{
-            bcrypt.compare(req.body.password, user.password, function(err, result){
-                if(result)
-                    res.send('User authenticated');
-                else
-                res.status(500).send('Wrong password');
-            });
-        }
-        
-    })(req, res, next);
+    if(req.session.user)
+    {
+        res.send({message: `Welcome back ${req.session.user.username}!`, user: req.session.user});
+    }
+    else{
+        passport.authenticate('local', {session: false}, function(e, user, info){
+            if(e){
+                res.send(e.message);
+            }
+    
+            if(!user){
+                res.status(500).send({message: 'User does not exist', user: null});
+            }
+            else{
+                bcrypt.compare(req.body.password, user.password, function(err, result){
+                    if(result){
+                        req.session.user = user;
+                        res.send({message: 'User authenticated', user: user});
+                    }
+                    else
+                    res.status(500).send({message: 'Wrong password', user: null});
+                });
+            }
+            
+        })(req, res, next);
+    }
 });
 
 router.post('/create', function(req, res){
